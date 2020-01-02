a36a72fd4487cd6f0b2cb2d91dcbf6822bf091e9
sunnyRK <radadiyasunny970@gmail.com.com>
2020-01-02 11:52:01 +0530

    Modified code of erc20 exchange module

--- src/scenarios/fill_order_erc20.ts
+++ src/scenarios/fill_order_erc20.ts
@@ -9,35 +9,16 @@ import { DECIMALS, NULL_ADDRESS, NULL_BYTES, UNLIMITED_ALLOWANCE_IN_BASE_UNITS, 
 import { providerEngine } from '../provider_engine';
 import { calculateProtocolFee, getRandomFutureDateInSeconds } from '../utils';
 
-/**
- * In this scenario, the maker creates and signs an order for selling ZRX for WETH.
- * The taker takes this order and fills it via the 0x Exchange contract.
- */
 export async function scenarioAsync(): Promise<void> {
-    // await runMigrationsOnceIfRequiredAsync();
-    // PrintUtils.printScenario('Fill Order');
-    // Initialize the ContractWrappers, this provides helper functions around calling
-    // 0x contracts as well as ERC20/ERC721 token contracts on the blockchain
     const contractWrappers = new ContractWrappers(providerEngine, { chainId: NETWORK_CONFIGS.chainId });
-    // Initialize the Web3Wrapper, this provides helper functions around fetching
-    // account information, balances, general contract logs
+    
     const web3Wrapper = new Web3Wrapper(providerEngine);
     const [maker, taker] = await web3Wrapper.getAvailableAddressesAsync();
     const zrxTokenAddress = contractWrappers.contractAddresses.zrxToken;
     const etherTokenAddress = contractWrappers.contractAddresses.etherToken;
-    // const printUtils = new PrintUtils(
-    //     web3Wrapper,
-    //     contractWrappers,
-    //     { maker, taker },
-    //     { WETH: etherTokenAddress, ZRX: zrxTokenAddress },
-    // );
-    // printUtils.printAccounts();
 
-    // the amount the maker is selling of maker asset
     const makerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(1), DECIMALS);
-    // the amount the maker wants of taker asset
     const takerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(0.01), DECIMALS);
-    // 0x v2 uses hex encoded asset data strings to encode all the information needed to identify an asset
     const makerAssetData = await contractWrappers.devUtils.encodeERC20AssetData(zrxTokenAddress).callAsync();
     const takerAssetData = await contractWrappers.devUtils.encodeERC20AssetData(etherTokenAddress).callAsync();
     let txHash;
@@ -48,27 +29,18 @@ export async function scenarioAsync(): Promise<void> {
     const makerZRXApprovalTxHash = await erc20Token
         .approve(contractWrappers.contractAddresses.erc20Proxy, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
         .sendTransactionAsync({ from: maker });
-    // await printUtils.awaitTransactionMinedSpinnerAsync('Maker ZRX Approval', makerZRXApprovalTxHash);
 
     // Allow the 0x ERC20 Proxy to move WETH on behalf of takerAccount
     const etherToken = contractWrappers.weth9;
     const takerWETHApprovalTxHash = await etherToken
         .approve(contractWrappers.contractAddresses.erc20Proxy, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
         .sendTransactionAsync({ from: taker });
-    // await printUtils.awaitTransactionMinedSpinnerAsync('Taker WETH Approval', takerWETHApprovalTxHash);
 
     // Convert ETH into WETH for taker by depositing ETH into the WETH contract
     const takerWETHDepositTxHash = await etherToken.deposit().sendTransactionAsync({
         from: taker,
         value: takerAssetAmount,
     });
-    // await printUtils.awaitTransactionMinedSpinnerAsync('Taker WETH Deposit', takerWETHDepositTxHash);
-
-    // PrintUtils.printData('Setup', [
-    //     ['Maker ZRX Approval', makerZRXApprovalTxHash],
-    //     ['Taker WETH Approval', takerWETHApprovalTxHash],
-    //     ['Taker WETH Deposit', takerWETHDepositTxHash],
-    // ]);
 
     // Set up the Order and fill it
     const randomExpiration = getRandomFutureDateInSeconds();
@@ -94,12 +66,6 @@ export async function scenarioAsync(): Promise<void> {
         takerFee: ZERO,
     };
 
-    // printUtils.printOrder(order);
-
-    // Print out the Balances and Allowances
-    // await printUtils.fetchAndPrintContractAllowancesAsync();
-    // await printUtils.fetchAndPrintContractBalancesAsync();
-
     // Generate the order hash and sign it
     const signedOrder = await signatureUtils.ecSignOrderAsync(providerEngine, order, maker);
 
@@ -120,13 +86,8 @@ export async function scenarioAsync(): Promise<void> {
             ...TX_DEFAULTS,
             value: calculateProtocolFee([signedOrder]),
         });
-    // txReceipt = await printUtils.awaitTransactionMinedSpinnerAsync('fillOrder', txHash);
-    // printUtils.printTransaction('fillOrder', txReceipt, [['orderHash', orderHash]]);
-
-    // Print the Balances
-    // await printUtils.fetchAndPrintContractBalancesAsync();
-
-    // Stop the Provider Engine
+    console.log(order);
+    console.log(txHash);
     providerEngine.stop();
 }
 
