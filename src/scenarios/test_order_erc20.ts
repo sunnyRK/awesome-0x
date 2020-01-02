19471610ed9a3f48f55536eb92a0259ae20828da
sunnyRK <radadiyasunny970@gmail.com.com>
2020-01-02 12:03:00 +0530

    Modified for all erc20 tokens

--- src/scenarios/test_order_erc20.ts
+++ src/scenarios/test_order_erc20.ts
@@ -1,108 +1,111 @@
-// import { 
-//     assetDataUtils,
-//     BigNumber,
-//     ContractWrappers,
-//     ERC20TokenContract,
-//     generatePseudoRandomSalt,
-//     Order,
-//     orderHashUtils,
-//     signatureUtils,
-//  } from "0x.js";
-// import { Web3Wrapper } from "@0x/web3-wrapper";
+import { ContractWrappers, ERC20TokenContract, OrderStatus } from '@0x/contract-wrappers';
+import { generatePseudoRandomSalt, Order, signatureUtils } from '@0x/order-utils';
+import { BigNumber } from '@0x/utils';
+import { Web3Wrapper } from '@0x/web3-wrapper';
 
-// import { NETWORK_CONFIGS, TX_DEFAULS } from "../configs";
-// import { DECIMALS, ZERO, NULL_ADDRESS, UNLIMITED_ALLOWANCE_IN_BASE_UNITS } from "../constants";
-// import { contractAddresses } from "../contracts";
-// // import { PrintUtils } from "../print_utils";
-// import { providerEngine } from "../provider_engine";
-// import { getRandomFutureInSeconds } from "../utils";
+import { NETWORK_CONFIGS, TX_DEFAULTS } from '../configs';
+import { DECIMALS, NULL_ADDRESS, NULL_BYTES, UNLIMITED_ALLOWANCE_IN_BASE_UNITS, ZERO } from '../constants';
+// import { PrintUtils } from '../print_utils';
+import { providerEngine } from '../provider_engine';
+import { calculateProtocolFee, getRandomFutureDateInSeconds } from '../utils';
 
-// export async function scenarioAsync(): Promise<void> {
-//     //
-//     //
-
-//     const contractWrappers = new ContractWrappers(providerEngine, { networkId: NETWORK_CONFIGS.networkId});
-//     const web3Wrapper = new Web3Wrapper(providerEngine);
-//     const [maker, taker, matcherAccount] = await web3Wrapper.getAvailableAddressesAsync();
-//     const zrxTokenAddress = contractAddresses.zrxToken;
-//     // const etherTokenAddress = contractAddresses.etherToken;
-//     // console.log(maker);
-//     // console.log(taker);
-//     // console.log(matcherAccount);    
-//     const daiTokenAddress = '0xC4375B7De8af5a38a93548eb8453a498222C4fF2';
-//     // console.log(daiTokenAddress);
-
-//     const makerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(1), DECIMALS);
-//     const takerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(1), DECIMALS);
-
-//     const makerAssetData = assetDataUtils.encodeERC20AssetData(daiTokenAddress);
-//     const takerAssetData = assetDataUtils.encodeERC20AssetData(zrxTokenAddress);
-//     let txHash;
-//     let txReciept;
-
-//     const daiToken = new ERC20TokenContract(daiTokenAddress, providerEngine);
-//     const zrxToken = new ERC20TokenContract(zrxTokenAddress, providerEngine);
-
-//     const makerDAIApprovalTxHash = await daiToken.approve.validateAndSendTransactionAsync(
-//         contractAddresses.erc20Proxy,
-//         UNLIMITED_ALLOWANCE_IN_BASE_UNITS,
-//         { from: maker },
-//     );
-//     //
-
-//     const takerZRXApprovalTxHash = await zrxToken.approve.validateAndSendTransactionAsync(
-//         contractAddresses.erc20Proxy,
-//         UNLIMITED_ALLOWANCE_IN_BASE_UNITS,
-//         { from: taker },
-//     );
-
-//     const randomExpiration = getRandomFutureInSeconds();
-//     const exchangeAddress = contractAddresses.exchange;
-
-//     const order: Order = {
-//         exchangeAddress,
-//         makerAddress: maker,
-//         takerAddress: taker,
-//         senderAddress: NULL_ADDRESS,
-//         feeRecipientAddress: NULL_ADDRESS,
-//         expirationTimeSeconds: randomExpiration,
-//         salt: generatePseudoRandomSalt(),
-//         makerAssetAmount,
-//         takerAssetAmount,
-//         makerAssetData,
-//         takerAssetData,
-//         makerFee: ZERO,
-//         takerFee: ZERO,
-//     };
-
-//     const orderHashHex = orderHashUtils.getOrderHashHex(order);
-//     const signature = await signatureUtils.ecSignHashAsync(providerEngine, orderHashHex, maker);
-//     const signedOrder =  { ...order, signature };   
-//     // console.log(orderHashHex);
-
-//     txHash = await contractWrappers.exchange.fillOrder.validateAndSendTransactionAsync(
-//         signedOrder,
-//         takerAssetAmount,
-//         signedOrder.signature,
-//         { from: taker, gas: TX_DEFAULS.gas },
-//     );
-//     console.log(txHash);
-//     console.log(order);
+export async function scenarioAsync(): Promise<void> {
+    const contractWrappers = new ContractWrappers(providerEngine, { chainId: NETWORK_CONFIGS.chainId });
     
-//     const orderInfo = await contractWrappers.exchange.getOrderInfo.callAsync(order);
-//     // console.log(orderInfo);
-//     providerEngine.stop();
-// }
+    const web3Wrapper = new Web3Wrapper(providerEngine);
+    const [maker, taker] = await web3Wrapper.getAvailableAddressesAsync();
+    const zrxTokenAddress = contractWrappers.contractAddresses.zrxToken;
+    // const etherTokenAddress = contractWrappers.contractAddresses.etherToken;
 
-// void (async () => {
-//     try{
-//         if (!module.parent) {
-//             await scenarioAsync();
-//         }
-//     } catch (e) {
-//         console.log(e);
-//         providerEngine.stop();
-//         process.exit(1);
-//     }
-// })();
+    const daiTokenAddress = '0xC4375B7De8af5a38a93548eb8453a498222C4fF2';
 
+    const makerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(1), DECIMALS);
+    const takerAssetAmount = Web3Wrapper.toBaseUnitAmount(new BigNumber(1), DECIMALS);
+    const makerAssetData = await contractWrappers.devUtils.encodeERC20AssetData(daiTokenAddress).callAsync();
+    const takerAssetData = await contractWrappers.devUtils.encodeERC20AssetData(zrxTokenAddress).callAsync();
+    let txHash;
+    let txReceipt;
+
+    // Allow the 0x ERC20 Proxy to move ZRX on behalf of makerAccount
+    const erc20Token1 = new ERC20TokenContract(daiTokenAddress, providerEngine);
+    const makerDAIApprovalTxHash = await erc20Token1
+        .approve(contractWrappers.contractAddresses.erc20Proxy, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
+        .sendTransactionAsync({ from: maker });
+
+    const erc20Token2 = new ERC20TokenContract(zrxTokenAddress, providerEngine);
+    const takerZRXApprovalTxHash = await erc20Token2
+        .approve(contractWrappers.contractAddresses.erc20Proxy, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
+        .sendTransactionAsync({ from: taker });
+
+    // Allow the 0x ERC20 Proxy to move WETH on behalf of takerAccount
+    // const etherToken = contractWrappers.weth9;
+    // const takerWETHApprovalTxHash = await etherToken
+    //     .approve(contractWrappers.contractAddresses.erc20Proxy, UNLIMITED_ALLOWANCE_IN_BASE_UNITS)
+    //     .sendTransactionAsync({ from: taker });
+
+    // Convert ETH into WETH for taker by depositing ETH into the WETH contract
+    // const takerWETHDepositTxHash = await etherToken.deposit().sendTransactionAsync({
+    //     from: taker,
+    //     value: takerAssetAmount,
+    // });
+
+    // Set up the Order and fill it
+    const randomExpiration = getRandomFutureDateInSeconds();
+    const exchangeAddress = contractWrappers.contractAddresses.exchange;
+
+    // Create the order
+    const order: Order = {
+        chainId: NETWORK_CONFIGS.chainId,
+        exchangeAddress,
+        makerAddress: maker,
+        takerAddress: NULL_ADDRESS,
+        senderAddress: NULL_ADDRESS,
+        feeRecipientAddress: NULL_ADDRESS,
+        expirationTimeSeconds: randomExpiration,
+        salt: generatePseudoRandomSalt(),
+        makerAssetAmount,
+        takerAssetAmount,
+        makerAssetData,
+        takerAssetData,
+        makerFeeAssetData: NULL_BYTES,
+        takerFeeAssetData: NULL_BYTES,
+        makerFee: ZERO,
+        takerFee: ZERO,
+    };
+
+    // Generate the order hash and sign it
+    const signedOrder = await signatureUtils.ecSignOrderAsync(providerEngine, order, maker);
+
+    const [
+        { orderStatus, orderHash },
+        remainingFillableAmount,
+        isValidSignature,
+    ] = await contractWrappers.devUtils.getOrderRelevantState(signedOrder, signedOrder.signature).callAsync();
+    if (orderStatus === OrderStatus.Fillable && remainingFillableAmount.isGreaterThan(0) && isValidSignature) {
+        // Order is fillable
+    }
+
+    // Fill the Order via 0x Exchange contract
+    txHash = await contractWrappers.exchange
+        .fillOrder(signedOrder, takerAssetAmount, signedOrder.signature)
+        .sendTransactionAsync({
+            from: taker,
+            ...TX_DEFAULTS,
+            value: calculateProtocolFee([signedOrder]),
+        });
+    console.log(order);
+    console.log(txHash);
+    providerEngine.stop();
+}
+
+void (async () => {
+    try {
+        if (!module.parent) {
+            await scenarioAsync();
+        }
+    } catch (e) {
+        console.log(e);
+        providerEngine.stop();
+        process.exit(1);
+    }
+})();
